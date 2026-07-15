067ac645e17d52b93d6d0b5474c1a4ff94d1b6b3
malbimoveis-star <malbimoveis@gmail.com>
2026-07-14 22:22:21 -0300

    Corrigindo blog.js para Express em inglês

--- backend/blog.js
+++ backend/blog.js
@@ -2,17 +2,14 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-// Página principal do blog
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/blog.html'));
+  res.sendFile(path.join(__dirname, '../frontend/blog.html'));
 });
 
-// Criar novo artigo (exemplo simples)
 router.post('/novo', (req, res) => {
-    const { titulo, conteudo } = req.body;
-    // Aqui futuramente você salvará no banco de dados
-    console.log('Nova postagem:', titulo, conteudo);
-    res.redirect('/admin/blog');
+  const { titulo, conteudo } = req.body;
+  console.log('Nova postagem:', titulo, conteudo);
+  res.redirect('/admin/blog');
 });
 
 module.exports = router;
