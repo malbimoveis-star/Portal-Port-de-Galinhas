85fe1d974c3d9ab9134d47c71c0bce06328c67a2
malbimoveis-star <malbimoveis@gmail.com>
2026-08-10 15:38:14 -0300

    Update home.js

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -54,6 +54,15 @@ document.addEventListener("DOMContentLoaded", async () => {
 
       loop: true,
 
+      spaceBetween: 20,
+
+      slidesPerView: 1,
+
+      breakpoints: {
+        640: { slidesPerView: 2 },
+        1000: { slidesPerView: 3 }
+      },
+
       pagination: {
         el: ".swiper-pagination",
         clickable: true
